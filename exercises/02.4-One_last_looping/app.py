b412ba9ac6e301996719aee5f50257c44f5ace3d
rroussmolina <rroussmolina@gmail.com>
2021-03-31 03:02:58 +0000

    first commit

--- exercises/02.4-One_last_looping/app.py
+++ exercises/02.4-One_last_looping/app.py
@@ -1,4 +1,12 @@
 my_sample_list = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fernando','Cesco','Bart','Annie']
 
 #Your code here:
+my_sample_list[1] = "Steven"
+my_sample_list[-1] ="Pepe"
+my_sample_list[0] = my_sample_list[2]+my_sample_list[4]
 
+#my_new_list = my_sample_list[::-1]
+#print(my_new_list)
+
+for x in reversed(my_sample_list):
+    print(x)
